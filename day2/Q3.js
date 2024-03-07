b77bd9a4a35b095c6882d065bb1466681d3aa354
Hooria Osama <159541875+Mrssheikhosama@users.noreply.github.com>
2024-03-07 20:03:07 +0500

    Add files via upload

---
+++ day2/Q3.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+
+Q.4 , Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including
+the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+*/
+const myQuote = ` Allama Iqbal once said, "Failure is not fatal until we surrender;
+ trying again is the key to glorious victory." `;
+console.log(myQuote);
---
+++ day2/Q4.js
@@ -0,0 +1,8 @@
+/*
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called
+famous_person. Then compose your message and store it in a new variable called message.
+ Print your message.
+ */
+var famousPerson = "Allama Iqbal";
+var quote = "".concat(famousPerson, " once said, \"Allama Iqbal once said, \"Failure is not fatal until we \n surrender trying again is the key to glorious victory.\"");
+console.log(quote);
