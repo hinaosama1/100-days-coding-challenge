39a037d7ce4920d9f9d8e176477160e6bcce5810
Hooria Osama <159541875+Mrssheikhosama@users.noreply.github.com>
2024-03-07 20:33:24 +0500

    Add files via upload

---
+++ day3/Q7.ts
@@ -0,0 +1,9 @@
+/* Number Eight: Write addition, subtraction, multiplication, and division operations that 
+each result in the number 8. Be sure to enclose your operations in print statements to see the result*/
+
+const addition = 4 + 4;
+const subtraction = 10 - 2;
+const multiplication = 2 * 4;
+const division = 32 / 4;
+
+console.log(addition, subtraction, multiplication, division);
---
+++ day3/Q9.ts
@@ -0,0 +1,9 @@
+/* 
+Favorite Number: Store your favorite number in a variable. Then, using that variable, create a 
+message that reveals your favorite number. Print that message.*/
+
+const favnum = 5;
+const MessAge = `My favourite Number is ${favnum}`;
+console.log(MessAge);
+
+
