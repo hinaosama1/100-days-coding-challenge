b7e43e6696a0941888d5ce00c3982294dd95aa14
Hooria Osama <159541875+Mrssheikhosama@users.noreply.github.com>
2024-03-07 19:53:16 +0500

    Q1.js

---
+++ day 1/Q2.js
@@ -0,0 +1,7 @@
+/*
+ Store a person’s name in a variable, and print a message to that person.
+  Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+*/
+var names = "Hina Azhar";
+console.log("Hello ".concat(names, ", would you like to learn some python today?"));
