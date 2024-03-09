034325fe564b154345f77283c682fc0994d38db5
Hooria Osama <159541875+Mrssheikhosama@users.noreply.github.com>
2024-03-09 09:15:11 +0500

    Add files via upload

---
+++ day 8/Q22.js
@@ -0,0 +1,12 @@
+/*
+Intentional Error: If you haven’t received an array index error in one of your programs yet, try to
+make one happen. Change an index in one of your programs to produce an index error. Make
+sure you correct the error before closing the program*/
+var myarray = [25, "hooria", 23, "car"];
+console.log(myarray[0]);
+console.log(myarray[1]);
+console.log(myarray[2]);
+// producing an index error
+console.log(myarray[4]);
+//finishing the error
+console.log(myarray[3]);
